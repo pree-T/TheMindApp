e34c239ad0467d1fc2d64777162fac427b33eb9d
pree-T <miss.preetisarkar0107@gmail.com>
2021-03-22 08:58:15 +0530

    An interesting Mind Game App, sudoku game almost done

--- app/src/main/java/com/example/newapplication/Sudoku.java
+++ app/src/main/java/com/example/newapplication/Sudoku.java
@@ -2,13 +2,27 @@ package com.example.newapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.newapplication.view.sudoku_game;
 
 public class Sudoku extends AppCompatActivity {
+    Button startbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
+        startbtn=findViewById(R.id.buttonsudo);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), sudoku_game.class);
+                startActivity(intent);
+            }
+        });
     }
 }
